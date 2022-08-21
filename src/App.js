3d173ff08a86196073f76d41884539b108988eb5
Luciano Ruiz <lucianoruizm993@gmail.com>
2022-08-21 00:46:14 -0300

    avance: filtro

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import React, { useState, useEffect } from "react";
+
+import Search from "./components/Search/Search";
+import Card from "./components/Card/Card";
+import Pagination from "./components/Pagination/Pagination";
+import Filter from "./components/Filter/Filter";
+import Navbar from "./components/Navbar/Navbar";
+
 
 function App() {
+
+  let [pageNumber, updatePageNumber] = useState(1);
+  let [search, setSearch] = useState("");
+
+  let [fetchedData, updateFetchedData] = useState([]);
+  let { info, results } = fetchedData;
+
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}`;
+
+  useEffect(() => {
+    (async function () {
+      let data = await fetch(api).then((res) => res.json());
+      updateFetchedData(data);
+    })();
+  }, [api]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+       <h1 className="text-center mb-3">Characters</h1>
+       <Search setSearch={setSearch} updatePageNumber={updatePageNumber} />
+       <div className="container">
+       <div className="row">
+         Filter component will be placed here
+         <div className="col-lg-8 col-12">
+           <div className="row">
+             <Card results={results} />
+           </div>
+         </div>
+       </div>
+       </div>
+       <Pagination 
+         info={info}
+         pageNumber={pageNumber}
+         updatePageNumber={updatePageNumber}
+        />
+  </div>
   );
 }
 
